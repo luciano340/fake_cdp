c49858e0acb1d1628b958d06c57c3b6c42f61bba
Luciano Romão <lucianosouzaromao@gmail.com>
2025-01-31 16:52:32 -0300

    feat: add kafka broker

--- consumer.py
+++ consumer.py
@@ -1,6 +1,8 @@
+import time
 from data_streaming.data_streaming_interface import DataStreamingInterface
 
 
 def consumer(broker: DataStreamingInterface) -> None:
         for msg in broker.recv():
             print("Mensangem recebida do broker!", msg.json())
+            time.sleep(1)
--- data_streaming/data_streaming.py
+++ data_streaming/data_streaming.py
@@ -1,17 +1,31 @@
-from queue import Queue
+import json
 from typing import Iterator
+from kafka import KafkaConsumer, KafkaProducer
 from DTO.events_dto import EventsDto
-from data_streaming.data_streaming_interface import T, DataStreamingInterface
-
+from data_streaming.data_streaming_interface import T, DataStreamingInterface 
 
 class DataStreaming(DataStreamingInterface):
-    def __init__(self):
-        self.__broker = Queue()
+    def __init__(self, group_id: str = "Estudos",  bootstrap_servers: str = 'localhost:9092' , topic: str = 'eventos_clientes'):
+        self.topic = topic
+        self.__producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+        )
+        self.__consumer = KafkaConsumer(
+            topic,
+            bootstrap_servers=bootstrap_servers,
+            group_id=group_id,
+            value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+            auto_offset_reset="earliest",
+        )
 
     def send(self, msg: EventsDto) -> None:
-        self.__broker.put(msg)
+        self.__producer.send(self.topic, msg.json())
 
     def recv(self) -> Iterator[T]:
-        while True:
-            msg = self.__broker.get()
-            yield msg
+        for msg in self.__consumer:
+            yield EventsDto(**msg.value)
+    
+    def close(self):
+        self.__producer.close()
+        self.__consumer.close()
